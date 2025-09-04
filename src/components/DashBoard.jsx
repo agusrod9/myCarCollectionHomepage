12554253c75e46124b30e0f75fc2ff58d6a2ded6
Agustin <agusrod9@gmail.com>
2025-09-04 18:25:45 -0300

    navegacion en dashboard por terminar

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -7,8 +7,8 @@ export function DashBoard(){
                 <DashBoardItem text="Notificaciones" className = "dbItem notif" imgUrl= "src/assets/react.svg" />
                 <DashBoardItem text="Métricas" className = "dbItem metrics" imgUrl= "src/assets/react.svg"/>
                 <DashBoardItem text="WishList" className = "dbItem wishlist"imgUrl= "src/assets/react.svg"/>
-                <DashBoardItem text="perfil" className = "dbItem perfil" imgUrl= "src/assets/react.svg" />
-                <DashBoardItem text="agregar auto" className = "dbItem newcar"imgUrl= "src/assets/react.svg"/>
+                <DashBoardItem text="perfil" className = "dbItem perfil" imgUrl= "src/assets/react.svg" nav="/profile"/>
+                <DashBoardItem text="agregar auto" className = "dbItem newcar"imgUrl= "src/assets/react.svg" nav="/newCar"/>
             </div>
 
         </section>
--- src/components/DashBoardItem.jsx
+++ src/components/DashBoardItem.jsx
@@ -1,8 +1,10 @@
 import './DashBoardItem.css'
+import { useNavigate } from 'react-router'
 
-export function DashBoardItem({text, className, imgUrl}){
+export function DashBoardItem({text, className, imgUrl, nav}){
+    const navigate = useNavigate();
     return(
-        <div className={className} onClick={()=>{console.log( "Click en " +className)}} onMouseEnter={()=>{console.log("hover")}} onMouseLeave= {()=>{console.log("chau hover")}}>
+        <div className={className} onClick={()=>{navigate(nav)}}>
             <p>{text}</p>
             <img src={imgUrl}></img>
         </div>
