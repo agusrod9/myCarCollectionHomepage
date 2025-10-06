c63885efedd2c6ed257a386d8847abde7f1bbbc2
Agustin <agusrod9@gmail.com>
2025-10-06 14:01:12 -0300

    mods en addcarForm

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,10 +1,10 @@
 import './CarCard.css'
 
 export function CarCard({car, containerClass, infoClass, includeManuf}){
+    
     return(
-        
         <div className={containerClass}>
-            <img src={car.img_urls[0]}/>
+            {car.img_urls.length<1 ? <img src='/public/v2.png' /> : <img src={car.img_urls[0] || ""}/>}
             <div className={infoClass}>
                 <p>{car.carMake} {car.carModel}</p>
                 <p>{car.carColor}</p>
