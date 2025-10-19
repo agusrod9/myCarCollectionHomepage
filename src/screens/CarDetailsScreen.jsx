5be2bdabde53e888d407c631fc2a1b784ee2c1e9
Agustin <agusrod9@gmail.com>
2025-10-19 11:29:44 -0300

    CARDETAILSSCREEN - Implement info side basic structure and funcitonality - TODO backend calling

--- src/screens/CarDetailsScreen.jsx
+++ src/screens/CarDetailsScreen.jsx
@@ -67,72 +67,51 @@ export function CarDetailsScreen(){
                     <div className={styles.imageContainer}>
                         <img src={car.img_urls[viewingImageIndex]} alt="" className={styles.imgDisplayed} />
                     </div>
+                    {carImgCount>1 && viewingImageIndex>0 ?
                     <div className={styles.navLeft}>
-                        <ChevronLeftCircle size={36} onClick={prevImageIndex}/>
+                        <ChevronLeftCircle size={36} onClick={prevImageIndex}/> 
                     </div>
+                    : null}
+                    {carImgCount>1 && viewingImageIndex<carImgCount-1 ?
                     <div className={styles.navRight}>
-                        <ChevronRightCircle size={36} onClick={nextImageIndex}/>
+                       <ChevronRightCircle size={36} onClick={nextImageIndex}/>
                     </div>
+                    : null}
+                    {carImgCount>1?
+                    <p className={styles.imageCountIndicator}>{`${viewingImageIndex+1}/${carImgCount}`}</p>
+                    :null}
                 </div>
                 <div className={styles.infoContainer}>
                     <label htmlFor="carMake">Make</label>
-                    <div className={styles.inputContainer}>
-                        <input name='carMake' type="text" value={editableCar.carMake} className={styles.dataInput} disabled={!editingFields.carMake} onChange={(e)=>{setEditableCar(prev=>({...prev, carMake: e.target.value}))}}/>
+                    <div className={editingFields.carMake ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
+                        <input name='carMake' type="text" value={editableCar.carMake} className={editingFields.carMake ? `${styles.dataInput} ${styles.editingMode}` : styles.dataInput} disabled={!editingFields.carMake} onChange={(e)=>{setEditableCar(prev=>({...prev, carMake: e.target.value}))}}/>
                         {editingFields.carMake 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{carMake, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, carMake:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, carMake:true}))}/>
                         }
                     </div>
                     <label htmlFor="carModel">Model</label>
-                    <div className={styles.inputContainer}>
-                        <input name='carModel' type="text" value={editableCar.carModel} className={styles.dataInput} disabled={!editingFields.carModel} onChange={(e)=>{setEditableCar(prev=>({...prev, carModel: e.target.value }))}}/>
+                    <div className={editingFields.carModel ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
+                        <input name='carModel' type="text" value={editableCar.carModel} className={editingFields.carModel ? `${styles.dataInput} ${styles.editingMode}` : styles.dataInput} disabled={!editingFields.carModel} onChange={(e)=>{setEditableCar(prev=>({...prev, carModel: e.target.value }))}}/>
                         {editingFields.carModel 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{carModel, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, carModel:true}))}/>
-                        }
-                    </div>
-                    <label htmlFor="carYear">Year</label>
-                    <div className={styles.inputContainer}>
-                        <input name='carYear' type="number" min={anioMinimo} max={anioActual+1} value={editableCar.carYear} className={styles.dataInput} disabled={!editingFields.carYear} onChange={(e)=>{setEditableCar(prev=>({...prev, carYear: e.target.value }))}}/>
-                        {editingFields.carYear
-                        ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
-                            const{carYear, ...rest}=prev
-                            return rest
-                            })}
-                        /> 
-                        : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, carYear:true}))}/>
-                        }
-                    </div>
-                    <label htmlFor="manufacturer">Manufacturer</label>
-                    <div className={styles.inputContainer}>
-                        <input name='manufacturer' type="text" value={editableCar.manufacturer} className={styles.dataInput} disabled={!editingFields.manufacturer} onChange={(e)=>{setEditableCar(prev=>({...prev, manufacturer: e.target.value }))}}/>
-                        {editingFields.manufacturer 
-                        ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
-                            const{manufacturer, ...rest}=prev
-                            return rest
-                            })}
-                        /> 
-                        : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, manufacturer:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, carModel:true}))}/>
                         }
                     </div>
                     <label htmlFor="scale">Scale</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.scale ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <select name="scale" value={editableCar.scale} className={styles.dataInput} disabled={!editingFields.scale} onChange={(e)=>{setEditableCar(prev=>({...prev, scale: e.target.value }))}} >
                         <option value={""}></option>
                         {
@@ -145,59 +124,88 @@ export function CarDetailsScreen(){
                     </select>
                         {editingFields.scale 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{scale, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, scale:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, scale:true}))}/>
                         }
                     </div>
+                    <label htmlFor="carYear">Year</label>
+                    <div className={editingFields.carYear ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
+                        <input name='carYear' type="number" min={anioMinimo} max={anioActual+1} value={editableCar.carYear} className={styles.dataInput} disabled={!editingFields.carYear} onChange={(e)=>{setEditableCar(prev=>({...prev, carYear: e.target.value }))}}/>
+                        {editingFields.carYear
+                        ? 
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
+                            const{carYear, ...rest}=prev
+                            return rest
+                            })}
+                        /> 
+                        : 
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, carYear:true}))}/>
+                        }
+                    </div>
+                    <label htmlFor="manufacturer">Manufacturer</label>
+                    <div className={editingFields.manufacturer ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
+                        <input name='manufacturer' type="text" value={editableCar.manufacturer} className={styles.dataInput} disabled={!editingFields.manufacturer} onChange={(e)=>{setEditableCar(prev=>({...prev, manufacturer: e.target.value }))}}/>
+                        {editingFields.manufacturer 
+                        ? 
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
+                            const{manufacturer, ...rest}=prev
+                            return rest
+                            })}
+                        /> 
+                        : 
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, manufacturer:true}))}/>
+                        }
+                    </div>
+                    
                     <label htmlFor="series">Series</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.series ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <input name='series' type="text" value={editableCar.series} className={styles.dataInput} disabled={!editingFields.series} onChange={(e)=>{setEditableCar(prev=>({...prev, series: e.target.value }))}}/>
                         {editingFields.series 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{series, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, series:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, series:true}))}/>
                         }
                     </div>
                     <label htmlFor="series_num">Series Number</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.series_num ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <input name='series_num' type="text" value={editableCar.series_num} className={styles.dataInput} disabled={!editingFields.series_num} onChange={(e)=>{setEditableCar(prev=>({...prev, series_num: e.target.value }))}}/>
                         {editingFields.series_num 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{series_num, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, series_num:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, series_num:true}))}/>
                         }
                     </div>
                     <label htmlFor="price">Price</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.price ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <input name='price' type="number" min={0} value={editableCar.price} className={styles.dataInput} disabled={!editingFields.price} onChange={(e)=>{setEditableCar(prev=>({...prev, price: e.target.value }))}}/>
                         {editingFields.price 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{price, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, price:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, price:true}))}/>
                         }
                     </div>
                     <label htmlFor="opened">Package</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.opened ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <select name="opened" value={editableCar.opened} className={styles.dataInput} disabled={!editingFields.opened} onChange={(e)=>{setEditableCar(prev=>({...prev, opened: e.target.value }))}}>
                             <option value={""}></option>
                             <option value='opened'>Opened</option>
@@ -205,17 +213,17 @@ export function CarDetailsScreen(){
                         </select>
                         {editingFields.opened 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{opened, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, opened:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, opened:true}))}/>
                         }
                     </div>
                     <label htmlFor="collection">Collection</label>
-                    <div className={styles.inputContainer}>
+                    <div className={editingFields.collectionId ? `${styles.inputContainer} ${styles.editingMode}`: styles.inputContainer}>
                         <select name="collection" value={editableCar.collectionId} className={styles.dataInput} disabled={!editingFields.collectionId} onChange={(e)=>{setEditableCar(prev=>({...prev, collectionId: e.target.value }))}}>
                             <option value={""}></option>
                             {
@@ -232,28 +240,29 @@ export function CarDetailsScreen(){
                         </select>
                         {editingFields.collectionId 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{collectionId, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, collectionId:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, collectionId:true}))}/>
                         }
                     </div>
                     <label htmlFor="notes">Notes</label>
-                    <div className={`${styles.inputContainer} ${styles.notesContainer}`}>
+                    <div className={editingFields.notes ? `${styles.inputContainer} ${styles.notesContainer} ${styles.editingMode}`: `${styles.inputContainer} ${styles.notesContainer}`}>
                         <textarea name='notes' type="textarea" maxLength={140} value={editableCar.notes} className={`${styles.dataInput} ${styles.notesInput}`} disabled={!editingFields.notes} onChange={(e)=>{setEditableCar(prev=>({...prev, notes: e.target.value }))}}/>
                         {editingFields.notes 
                         ? 
-                        <Save onClick={()=>setEditingFields(prev=>{
+                        <Save size={30} onClick={()=>setEditingFields(prev=>{
                             const{notes, ...rest}=prev
                             return rest
                             })}
                         /> 
                         : 
-                        <Edit onClick={()=>setEditingFields(prev=>({...prev, notes:true}))}/>
+                        <Edit size={30} onClick={()=>setEditingFields(prev=>({...prev, notes:true}))}/>
                         }
+                    </div>
                         {
                             editingFields.notes 
                             ?
@@ -261,7 +270,6 @@ export function CarDetailsScreen(){
                             :
                             null
                         }
-                    </div>
                     {Object.keys(editingFields).length>1 ? <ActionBtn label={"Save all"} icon={<Save/>}/> : null}
                     
                 </div>
