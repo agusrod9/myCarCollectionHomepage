e51c044504b001a2e3bdaaeade92d1f12a5389c3
Agustin <agusrod9@gmail.com>
2025-12-11 09:46:35 -0300

    Added Add to favorites and Star icon to dashboard recent car cards

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -53,7 +53,7 @@ export function CarCard({car, acBtnClick, key, onFavoriteToggle}){
     return(
         <div className={styles.cardContainer} key={key}>
             <img src={car.img_urls[0] || placeholder} alt={`Picture of ${car.carMake} car.`} className={styles.cardImg}/>
-            <Star fill={car.isFavorite ? '#fff' : 'none'} className={styles.favoriteIcon} onClick={()=>handleAddToFavToggle(car)}/>
+            <Star size={30} color='#1458bd' fill={car.isFavorite ? '#3a7aec' : 'none'} className={styles.favoriteIcon} onClick={()=>handleAddToFavToggle(car)}/>
             <div className={styles.cardInfoContainer}>
                 <p className={styles.cardCarMake}>{car.carMake} {car.carModel}</p>
                 <p className={styles.cardCarScale}>{car.scale}</p>
--- src/components/DashBoardCard.jsx
+++ src/components/DashBoardCard.jsx
@@ -2,10 +2,53 @@ import { useContext, useEffect, useState } from "react"
 import { AppContext } from "../context/AppContext"
 import styles from './DashBoardCard.module.css'
 import { getAddedDateText } from "../utils/dates.util"
+import { Star } from "lucide-react"
+import toast from "react-hot-toast"
 
 export function DashBoardCard({car, containerClass, infoClass, includeManuf, onClick}){
-    const {placeholder} = useContext(AppContext)
+    const API_BASEURL = import.meta.env.VITE_API_BASEURL;
+    const {placeholder, setUserFavoritesCount, userFavoritesCount, setUserCollectedCars} = useContext(AppContext)
     const [addedDateInfo, setAddedDateInfo] = useState("")
+    const handleAddToFavToggle = async(car)=>{
+        let t;
+        let action = "";
+        if(car.isFavorite){
+            t = toast.loading("Removing from favorites...", {duration : 10000});
+            action = "remove"
+        }else{
+            action = "add"
+            t = toast.loading("Adding to favorites...", {duration : 10000});
+        }
+        const updateData = {
+            isFavorite : !car.isFavorite
+        }
+        const url = `${API_BASEURL}cars/${car._id}`
+        const opts = {
+            method : 'PUT',
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify(updateData)
+        }
+        const response = await fetch(url,opts);
+        if(response.status === 200){
+            action === "add" ? setUserFavoritesCount(userFavoritesCount+1) : setUserFavoritesCount(userFavoritesCount-1)
+            action === "add" ? 
+                toast.success(`${car.carMake} ${car.carModel} added to favorites!`, {duration:2000, id : t})
+                :
+                toast.success(`${car.carMake} ${car.carModel} removed from favorites!`, {duration:2000, id : t})
+            setUserCollectedCars(prev=> {
+                const updated = prev.map(c=> 
+                    c._id === car._id 
+                    ? {...c, isFavorite : !car.isFavorite}
+                    : c
+                )
+                
+                return updated
+            })
+        }else{
+            toast.error(`Error adding ${car.carMake} ${car.carModel} to favorites`, {duration:2000, id : t});
+        }
+    }
+
     useEffect(()=>{
         setAddedDateInfo(getAddedDateText(car.dateAdded))
     },[])
@@ -17,6 +60,11 @@ export function DashBoardCard({car, containerClass, infoClass, includeManuf, onC
                 {includeManuf ? <p className={styles.row}>{car.manufacturer}</p> : <p></p>}
                 <p className={styles.row}>{car.scale}</p>
                 <p className={`${styles.row} ${styles.lastRow}`}>{addedDateInfo}</p>
+                <Star size={22} color='#1458bd' fill={car.isFavorite ? '#3a7aec' : 'none'} className={styles.favoriteIcon} onClick={(e)=>{
+                        e.stopPropagation()
+                        handleAddToFavToggle(car)
+                    }
+                }/>
             </div>
         </div>
     )
